662ada6085cc65a00023c58b73f7ad650875bf6b
Alexander <duducensash@mail.ru>
2019-11-24 19:39:48 +0300

    commit

--- src/com/company/alexander/Choice.java
+++ src/com/company/alexander/Choice.java
@@ -12,7 +12,11 @@ public class Choice {
         Scanner in = new Scanner(System.in);
         switch (num) {
             case (1):
+                out("На сколько делать сдвиг?");
                 Caesar.caeser(text,in.nextInt());
+            case (2):
+                out("какой ключ?");
+
             default:
                 out("Вы ввели либо не число либо число которого нет в предложенном ");
                 choice(text,in.nextInt());
